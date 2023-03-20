d7d6929b27e177cd78dbf71c76bfb17e9cf36cc9
Khalilevre <1244592585@qq.com>
2023-03-20 19:58:24 +0800

    cfp320

--- rekognition_image_detection.py
+++ rekognition_image_detection.py
@@ -230,13 +230,14 @@ def usage_demo():
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     rekognition_client = boto3.client('rekognition')
-    street_scene_file_name = "./media/pexels-kaique-rocha-109919.jpg"
-    celebrity_file_name = "./media/pexels-pixabay-53370.jpg"
+    add_photo1="D:\文档\fork-of-320\1.jpg"
+    street_scene_file_name = "D:\文档\fork-of-320\pexels-kaique-rocha-109919.jpg"
+    celebrity_file_name = "D:\文档\fork-of-320\pexels-pixabay-53370.jpg"
     one_girl_url = 'https://dhei5unw3vrsx.cloudfront.net/images/source3_resized.jpg'
     three_girls_url = 'https://dhei5unw3vrsx.cloudfront.net/images/target3_resized.jpg'
     swimwear_object = boto3.resource('s3').Object(
         'console-sample-images-pdx', 'yoga_swimwear.jpg')
-    book_file_name = './media/backgroud.png'
+    book_file_name = 'D:\文档\fork-of-320\backgroud.png'
 
     street_scene_image = RekognitionImage.from_file(
         street_scene_file_name, rekognition_client)
